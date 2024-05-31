aa4e04ffd0317a6464058b6167a65a6689927eed
gitlakshay10 <lakshay@lancepaas.com>
2024-05-31 14:19:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     $('.accordian').click(function(){
+//       $(".a-child").removeClass('.a-child')
+//       $('.a-child').slideToggle('fast')
+//     })
+// })
+$(document).ready(function(){
+    
+    $('.accordian-item').click(function(){
+
+        $('.a-child').slideUp('fast');
+
+        if ($(this).next().is(':visible')) {
+            $(this).next().slideUp('fast');
+        } else {
+            $(this).next().slideDown('fast');
+        }
+    })
+})
